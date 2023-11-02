8358dc2d3e6dfc0e42113a5800c568038ad54b0f
aymendhieb1 <aymen.dhieb@esprit.tn>
2023-11-02 14:48:38 +0100

    connexion.h (fix)

---
+++ connexion.h
@@ -0,0 +1,16 @@
+#ifndef CONNECTION_H
+#define CONNECTION_H
+
+#include <QSqlDatabase>
+#include <QSqlError>
+#include <QSqlQuery>
+
+class Connexion
+{
+public:
+    Connexion();
+    bool createconnexion();
+    void fermerConnexion();
+};
+
+#endif // CONNECTION_H
