095816c9ca731175e193d196fd82c52670a7b4c0
aymendhieb1 <aymen.dhieb@esprit.tn>
2023-11-09 14:13:26 +0100

    v3 formation

--- formation.cpp
+++ formation.cpp
@@ -16,7 +16,7 @@ DATE_DE_FIN=QDate::currentDate();
 NB_PLACE=0;
 }
 
-formation::formation(int IDFOR ,QString TYPE_DE_FORMATION ,QString FORMATEUR ,QDate DATE_DE_DEBUT,QDate DATE_DE_FIN,QString DUREE_DE_LA_FORMATION,int NB_PLACE)
+formation::formation(int IDFOR ,QString FORMATEUR,QString TYPE_DE_FORMATION ,QDate DATE_DE_DEBUT,QDate DATE_DE_FIN,QString DUREE_DE_LA_FORMATION,int NB_PLACE)
 {
 
     this->IDFOR=IDFOR;
@@ -51,15 +51,15 @@ void formation::setnbplace(int NB_PLACE){this->NB_PLACE=NB_PLACE;}
     {
         QSqlQuery query;
 
-        query.prepare("INSERT INTO formation (IDFOR,FORMATEUR,TYPE_DE_FORMATION,DATE_DE_DEBUT,DATE_DE_FIN,DUREE_DE_LA_FORMATION,NB_PLACE) "
-                      "VALUES (:IDFOR, :FOR, :TYPE_DE_FORMATION, :DATE_DE_DEBUT, :DATE_DE_FIN, :DUREE_DE_LA_FORMATION, :NB_PLACE)");
+        query.prepare("INSERT INTO formation (IDFOR,FORMATEUR,TYPE,DATE_DE_DEBUT,DATE_DE_FIN,DUREE,NB_PLACE) "
+                      "VALUES (:IDFOR, :FORMATEUR, :TYPE, :DATE_DE_DEBUT, :DATE_DE_FIN, :DUREE, :NB_PLACE)");
 
         query.bindValue(":IDFOR", IDFOR);
-        query.bindValue(":FOR", FORMATEUR);
-        query.bindValue(":TYPE_DE_FORMATION", TYPE_DE_FORMATION);
+        query.bindValue(":FORMATEUR", FORMATEUR);
+        query.bindValue(":TYPE", TYPE_DE_FORMATION);
         query.bindValue(":DATE_DE_DEBUT", DATE_DE_DEBUT);
         query.bindValue(":DATE_DE_FIN",DATE_DE_FIN);
-        query.bindValue(":DUREE_DE_LA_FORMATION",DUREE_DE_LA_FORMATION);
+        query.bindValue(":DUREE",DUREE_DE_LA_FORMATION);
         query.bindValue(":NB_PLACE", NB_PLACE);
 
         return query.exec();
@@ -73,10 +73,10 @@ QSqlQueryModel* formation::afficher()
          model->setQuery("SELECT* FROM formation");
          model->setHeaderData(0, Qt::Horizontal,QObject:: tr("IDFOR"));
          model->setHeaderData(1, Qt::Horizontal,QObject:: tr("FORMATEUR"));
-         model->setHeaderData(2, Qt::Horizontal,QObject:: tr("TYPE_DE_FORMATION"));
+         model->setHeaderData(2, Qt::Horizontal,QObject:: tr("TYPE"));
          model->setHeaderData(3, Qt::Horizontal,QObject:: tr("DATE_DE_DEBUT"));
          model->setHeaderData(4, Qt::Horizontal,QObject:: tr("DATE_DE_FIN"));
-         model->setHeaderData(5, Qt::Horizontal,QObject:: tr("DUREE_DE_LA_FORMATION"));
+         model->setHeaderData(5, Qt::Horizontal,QObject:: tr("DUREE"));
          model->setHeaderData(6, Qt::Horizontal,QObject:: tr("NB_PLACE"));
 
          // Create a table view
@@ -86,6 +86,29 @@ QSqlQueryModel* formation::afficher()
             // Get the header view
             QHeaderView* headerView = tableView->horizontalHeader();
             headerView->setSectionResizeMode(QHeaderView::Stretch); // Optional: Adjust the section resize mode
+    return model;
+}
+
+QSqlQueryModel* formation::tri()
+{
+    QSqlQueryModel* model = new QSqlQueryModel();
+    model->setQuery("SELECT * FROM formation ORDER BY DATE_DE_DEBUT ASC"); // Query modified to order by DATE_DE_DEBUT in ascending order
+    model->setHeaderData(0, Qt::Horizontal, QObject::tr("IDFOR"));
+    model->setHeaderData(1, Qt::Horizontal, QObject::tr("FORMATEUR"));
+    model->setHeaderData(2, Qt::Horizontal, QObject::tr("TYPE"));
+    model->setHeaderData(3, Qt::Horizontal, QObject::tr("DATE_DE_DEBUT"));
+    model->setHeaderData(4, Qt::Horizontal, QObject::tr("DATE_DE_FIN"));
+    model->setHeaderData(5, Qt::Horizontal, QObject::tr("DUREE"));
+    model->setHeaderData(6, Qt::Horizontal, QObject::tr("NB_PLACE"));
+
+    // Create a table view
+    QTableView* tableView = new QTableView;
+    tableView->setModel(model);
+
+    // Get the header view
+    QHeaderView* headerView = tableView->horizontalHeader();
+    headerView->setSectionResizeMode(QHeaderView::Stretch);
+
     return model;
 }
 
@@ -109,3 +132,33 @@ bool formation::idExists(int id)
 
     return false;
 }
+
+bool formation::modifier(int IDFOR,QString formateur,QString type_de_formation,QString duree,QDate date_de_debut,QDate date_de_fin,int nb_place)
+{
+//,QString formateur,QString type_de_formation,QString duree,QDate DATE_DE_DEBUT,QDate DATE_DE_FIN,int NB_PLACE
+    QSqlQuery query;
+
+
+    query.prepare("UPDATE formation SET FORMATEUR=:FORMATEUR , TYPE=:TYPE, DATE_DE_DEBUT=:DATE_DE_DEBUT , DATE_DE_FIN=:DATE_DE_FIN ,DUREE=:DUREE ,NB_PLACE=:NB_PLACE where IDFOR=:IDFOR");
+    query.bindValue(":IDFOR", IDFOR);
+    query.bindValue(":FORMATEUR", formateur);
+    query.bindValue(":TYPE", type_de_formation);
+    query.bindValue(":DATE_DE_DEBUT", date_de_debut);
+    query.bindValue(":DATE_DE_FIN",date_de_fin);
+    query.bindValue(":DUREE",duree);
+    query.bindValue(":NB_PLACE", nb_place);
+
+
+      if (query.exec()) {
+          // Commit the transaction
+          QSqlDatabase::database().commit();
+          return true;
+      } else {
+          // Rollback the transaction in case of an error
+          QSqlDatabase::database().rollback();
+          return false;
+      }
+
+
+}
+
