f51505be17262a056c7ead29eb6081d1f93bc0bb
aymendhieb1 <aymen.dhieb@esprit.tn>
2023-11-09 14:17:19 +0100

    v3 mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -3,6 +3,32 @@
 
 #include <QMainWindow>
 #include "formation.h"
+#include <QFileDialog>
+#include <QPainter>
+#include <QPdfWriter>
+
+
+#include<QGraphicsDropShadowEffect>
+#include<QStackedWidget>
+#include <QApplication>
+#include <QLineEdit>
+#include <QWidget>
+#include <QPropertyAnimation>
+#include <QPalette>
+#include<QMessageBox>
+#include<QObject>
+#include<QIntValidator>
+#include<QSqlTableModel>
+#include <iostream>
+#include <QTableView>
+#include <QPropertyAnimation>
+#include <QLinearGradient>
+#include <QBrush>
+#include<QAbstractTableModel>
+#include <cstdlib>
+#include <QPrinter>
+#include <QTextDocument>
+#include<iostream>
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
@@ -16,24 +42,56 @@ public:
     ~MainWindow();
 
 private slots:
-    void on_bt_ajouter_clicked();
 
     void on_pushButton_clicked();
 
     void on_bt_return_clicked();
-
-    void on_pushButton_2_clicked();
 
     void on_ajouter_Canel_clicked();
 
     void on_ajouter_Confirm_clicked();
 
     void on_supprimer_clicked();
-
-    //void on_supprimer_clicked();
     
+    void on_ajouter_clicked();
+
+    void on_bt_modifier_clicked();
+    void updateUI(int );
+    void on_mconfirm_clicked();
+
+    void on_bt_mreturn_clicked();
+
+    void on_TRI_clicked();
+
+    void on_bt_rechercher_clicked();
+
+    void on_refresh_clicked();
+
+    void on_login_connecter_clicked();
+
+    void on_creer_confirmer_clicked();
+
+    void on_login_show_clicked();
+
+    void on_PDF_clicked();
+
+    void on_STATISTIQUE_clicked();
+    int countType(const QString& );
+
+
+void on_mannuler_clicked();
+
+
+
+void on_hide_clicked();
+
+void on_login_hide_clicked();
+
 private:
     Ui::MainWindow *ui;
     formation f;
+    void init();
+    void on_mid_currentIndexChanged(int index);
+
 };
 #endif // MAINWINDOW_H
