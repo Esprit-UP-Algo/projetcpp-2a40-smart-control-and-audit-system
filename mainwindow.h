b4a4368db673ffdfff0fb7c005196353661c99c7
aymendhieb <aymen.dhieb@esprit.tn>
2023-11-24 14:05:32 +0100

    mainwindow.h S11

--- mainwindow.h
+++ mainwindow.h
@@ -47,7 +47,7 @@ private slots:
 
     void on_bt_return_clicked();
 
-    void on_ajouter_Canel_clicked();
+    void on_ajouter_Cancel_clicked();
 
     void on_ajouter_Confirm_clicked();
 
@@ -56,14 +56,14 @@ private slots:
     void on_ajouter_clicked();
 
     void on_bt_modifier_clicked();
-    void updateUI(int );
+
     void on_mconfirm_clicked();
 
     void on_bt_mreturn_clicked();
 
     void on_TRI_clicked();
 
-    void on_bt_rechercher_clicked();
+ void on_texte_recherche_textChanged(const QString &text);
 
     void on_refresh_clicked();
 
@@ -75,7 +75,7 @@ private slots:
 
     void on_PDF_clicked();
 
-    void on_STATISTIQUE_clicked();
+    void STATISTIQUE();
     int countType(const QString& );
 
 
@@ -86,6 +86,37 @@ void on_mannuler_clicked();
 void on_hide_clicked();
 
 void on_login_hide_clicked();
+
+void on_acceuil_gestion_employe_clicked();
+
+void on_acceuil_gestion_entreprise_clicked();
+
+void on_acceuil_gestion_inspection_clicked();
+
+void on_acceuil_gestion_certificat_clicked();
+
+void on_acceuil_gestion_formation_clicked();
+
+void on_gestion_formation_deconnecter_clicked();
+
+void on_bt_historique_clicked();
+
+void onComboBoxhistorique(int index);
+
+void on_pushButton_2_clicked();
+
+void Notification();
+
+QString Message_Notification();
+void on_creer_return_clicked();
+
+void on_notification_system();
+
+void on_QUITTER_clicked();
+
+void on_FULLSCREEN_clicked();
+
+void on_yt_clicked();
 
 private:
     Ui::MainWindow *ui;
